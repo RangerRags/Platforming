5a2a87d1031e5d47d92c8317f8f62e2136004df7
Rags <zieglermicah@gmail.com>
2026-03-29 13:27:51 -0400

    improved collision

--- main.c
+++ main.c
@@ -6,6 +6,7 @@ int main(void) {
     InitWindow(800, 800, "Platform Game");
     int x=200, y=400;
     int ground=765;
+    bool onGround = false;
     int yV=0;
     SetTargetFPS(60);
     int platforms[10][10]={
@@ -23,56 +24,65 @@ int main(void) {
 
 
     while (!WindowShouldClose()) {
-        if (IsKeyDown(KEY_D)) {
-            x+=10;
-        }
-        if (IsKeyDown(KEY_A)) {
-            x-=10;
-        }
-        if (IsKeyPressed(KEY_W)) {
-            if (y>ground-25) {
-             yV=-15;
-            }
+        // 1. reset each frame
+        onGround = false;
+        ground = 765;
 
-        }
+        // 2. input
+        if (IsKeyDown(KEY_D)) x += 10;
+        if (IsKeyDown(KEY_A)) x -= 10;
 
-        if (y < ground) {
-            yV += 1;
-        }
-        ground =765;
-        if (y>=ground) {
-            y=ground-1;
-            yV=0;
-        }
+        // 3. apply gravity
+        if (!onGround) yV += 1;
 
-        for (int i=0;i<10;i++) {
-            for (int j=0;j<10;j++) {
-                if (platforms[i][j]==1) {
+        // 4. move
+        y += yV;
 
-                    if (x>=j*80-20&&x<=j*80+60&&y>=i*80-20) {
-                        ground=i*80-20;
-                        y=i*80-21;
+        // 5. platform collision
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (platforms[i][j] == 1) {
+                    // top collision
+                    if (x+20 >= j*80 && x-20 <= j*80+80 && y+20 >= i*80 && y < i*80) {
+                        ground = i*80;
+                        y = i*80 - 21;
+                        yV = 0;
+                        onGround = true;
                     }
-                    if (x>=j*80&&y >= i*80 && y <= i*80+80) {
-                        x=j*80-20-1;
+                    // side collision (left side of tile)
+                    if (x+20 >= j*80 && x < j*80 && y+20 >= i*80 && y-20 <= i*80+80) {
+                        x = j*80 - 21;
                     }
                 }
             }
         }
-        y+=yV;
+
+        // 6. floor collision
+        if (y >= ground) {
+            y = ground - 1;
+            yV = 0;
+            onGround = true;
+        }
+
+        // 7. jump (after onGround is known)
+        if (IsKeyPressed(KEY_W) && onGround) {
+            yV = -15;
+        }
+
+        // 8. draw
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawCircle(x,y,20,BLACK);
-        for (int i=0;i<10;i++) {
-            for (int j=0;j<10;j++) {
-                if (platforms[i][j]==1) {
-                    DrawRectangle(j*80,i*80,80,80,BLACK);
+        DrawCircle(x, y, 20, BLACK);
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (platforms[i][j] == 1) {
+                    DrawRectangle(j*80, i*80, 80, 80, BLACK);
                 }
             }
         }
         EndDrawing();
     }
 
-
+    CloseWindow();
     return 0;
 }
